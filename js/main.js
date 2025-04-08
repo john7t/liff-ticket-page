ba06a40abf64f0615cb3f002e6a9f64527fa1265
john7t <papal-pinch7t@icloud.com>
2025-04-08 22:58:23 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 const LIFF_ID = "2007223613-5OL4mpnA";
-const API_URL = "https://script.google.com/macros/s/AKfycbzgn1GP9NOv7dEA3-50B3eaU8QcLoDULVPpoCYFfVA/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbxbKNmC_pWv2ev9UoL4nhzJeqOtVOGGdRt_mTZwFkMDNZQOAcBasa_GwCOyrB8dwdV6Rw/exec";
 
 let userId = "";
 
